9e5e636f1a523a9d718c7fa0f8df7e89dd679a48
Andrew213 <mega.zip2011@gmail.com>
2020-06-07 11:59:11 +0300

    fixed and finished

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,7 @@ $(function () {
 
     $(".rate-star-product").rateYo({
         rating: 4.5,
-        starWidth: "12px",
+        starWidth: "13px",
         readOnly: true,
         spacing: "3px",
         multiColor: {
@@ -20,7 +20,7 @@ $(function () {
         }
     });
 
-    $('.weekly-slider').slick({
+    $('.weekly__slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         fade: true,
@@ -35,6 +35,7 @@ $(function () {
     $('.followers__inner').slick({
         slidesToShow: 3,
         slidesToScroll: 3,
+        slideWidth: 360,
         appendArrows: $('.followers__buttons'),
         prevArrow: '<button class="prev-arrow myarrow" type="button"><span class="lnr lnr-chevron-left"></span> </button>',
         nextArrow: '<button class="next-arrow myarrow" type="button"><span class="lnr lnr-chevron-right"></span></button>'
@@ -47,18 +48,50 @@ $(function () {
         slidesToScroll: 2,
         slideWidth: 555,
         slideMargin: 30,
-        appendArrows: $('.feedback-slider-arrows'),
+        appendArrows: $('.feedback__slider-arrows'),
         prevArrow: '<button class="prev-arrow feedback-arrow" type="button"><span class="lnr lnr-chevron-left"></span> </button>',
         nextArrow: '<button class="next-arrow feedback-arrow" type="button"><span class="lnr lnr-chevron-right"></span></button>'
 
 
     });
 
+
+    $('.products__title-btn').on('click', function () {
+        $(".products__title-list").toggleClass('list-active');
+    });
+
+
+    $('.products__title-link').on('click', function () {
+
+        $('.products__title-link--remove').removeClass('link-active');
+        $(this).addClass('link-active');
+    });
+
+
     $('.product-btn').on('click', function () {
 
-        $('.remove').removeClass('active');
+        $('.products__buttons--remove').removeClass('active');
         $(this).addClass('active');
     });
 
-    var mixer = mixitup('.products-inner__items');
+
+
+
+    // $(".feedback__item-text").each(function () {
+
+    //     var Height = 120;
+
+    //     if ($(this).height() > Height) {
+    //         $(".feedback__item").css({
+    //             'overflow': 'hidden',
+    //             'max-height': '305px',
+    //         });
+    //         $(this).find(".show-more").show();
+    //     }
+    // });
+
+
+
+
+    var mixer = mixitup('.products__items');
 });
